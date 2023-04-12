d9cc2096d0a8e0dada7c65a066e94256a73e6ed9
Opegbemi Ayodele Jacob <jacobay43@gmail.com>
2023-04-12 20:36:19 +0100

    Added allowed host *.vercel.app

--- emtrees_unofficial/settings.py
+++ emtrees_unofficial/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost','.vercel.app']
+ALLOWED_HOSTS = ['localhost','*.vercel.app']
 
 
 # Application definition
