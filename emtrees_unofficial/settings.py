8e89dd1512f19782ea6a049a779bfca557ebf581
Opegbemi Ayodele Jacob <jacobay43@gmail.com>
2023-04-13 09:40:25 +0100

    Preparing staticfiles for production env

--- emtrees_unofficial/settings.py
+++ emtrees_unofficial/settings.py
@@ -147,6 +147,7 @@ OPTIONS = {
 
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
     
--- emtrees_unofficial/urls.py
+++ emtrees_unofficial/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.sitemaps.views import sitemap 
 from emsite.sitemaps import CourseSitemap
+from django.conf import settings
+from django.conf.urls.static import static
 
 sitemaps = {'courses':CourseSitemap,}
 urlpatterns = [
@@ -24,3 +26,6 @@ urlpatterns = [
     path('',include('emsite.urls')),
     path('sitemap.xml',sitemap,{'sitemaps':sitemaps},name='django.contrib.sitemaps.views.sitemap')
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
